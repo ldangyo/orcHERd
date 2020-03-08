e3a7eeec78979a03e1252a386c9d2468e0ffbb48
ldangyo <ldangngy@sfu.ca>
2020-03-07 19:11:49 -0800

    Main File Added

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MainPage />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainPage from "./components/MainPage.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    MainPage
   }
-}
+};
 </script>
 
 <style>
---
+++ src/components/LoginPage.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="loginPage">
+    <div class="title">Welcome to Orchard</div>
+    <div class="loginCred">
+      <div class="label">
+        Username
+        <input type="text" id="username" />
+      </div>
+      <div class="label">
+        Password
+        <input type="text" id="password" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoginPage"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.loginCred {
+  padding-top: 2rem;
+}
+</style>
