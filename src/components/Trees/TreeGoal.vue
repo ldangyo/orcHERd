e6cb86544d76e339661d748ac94b794f62864229
ldangyo <ldangngy@sfu.ca>
2020-03-08 01:56:20 -0800

    added getters and setters

---
+++ src/components/Trees/TreeGoal.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="TreeGoal"></div>
+</template>
+/
+<script>
+export default {
+  //calls different tree states
+  name: "TreeGoal",
+  props: {
+    //Goal Name
+    //Goal Category
+    //Goal Progress
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.Tree {
+  padding-top: 2rem;
+}
+</style>
--- src/store/store.js
+++ src/store/store.js
@@ -5,26 +5,86 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
     state: {
-        savingGoals: [{ name: "Korea Vacation", amountSaved: "5", amountReq: "56", category: "Entertainment" }]
+        savingGoals: [
+            { name: "Korea Vacation2", amountSaved: "5", amountReq: "56", category: "Entertainment" },
+            { name: "Korea Vacation", amountSaved: "5", amountReq: "56", category: "Entertainment" },
+            { name: "Korea Vacation3", amountSaved: "5", amountReq: "56", category: "Entertainment" },
+            { name: "Korea Vacation3", amountSaved: "5", amountReq: "56", category: "Entertainment" },
+        ],
+        selectedGoalIndex: 0,
+        showGoalInfo: false,
 
     },
 
     getters: {
         getSavingGoals: state => {
             return state.savingGoals
+        },
+        getselectedGoalIndex: state => {
+            return state.selectedGoalIndex
+        },
+        getshowGoalInfo: state => {
+            return state.showGoalInfo
         }
-    },
-
-    mutations: {
-        setAddGoal(state, data) {
-            state.savingGoals.push(data)
-        }
-
     },
 
     actions: {
-        addGoal(state, data) {
-            state.commit("setAddGoal", data)
-        }
-    }
+        addGoal({ commit }, data) {
+            commit("setAddGoal", data)
+        },
+        //actions for goal Index
+        incrGoalIndex({ commit, state }) {
+            if (state.selectedGoalIndex + 1 < state.savingGoals.length - 1) {
+
+                commit("setIncrGoal")
+            } else {
+                commit("setGoalIndex", 0)
+
+            }
+            console.log(state.selectedGoalIndex)
+
+        },
+        decrGoalIndex({ commit, state }) {
+            if (state.selectedGoalIndex - 1 > -1) {
+                commit("setDecrGoal")
+            } else {
+                commit("setGoalIndex", state.savingGoals.length - 2)
+            }
+            console.log(state.selectedGoalIndex)
+
+        },
+
+        changeGoalIndex({ commit }, data) {
+            commit("setGoalIndex", data)
+        },
+
+        //action to show goal info
+        changeShowGoalInfo({ commit }, data) {
+            commit("setShowGoalInfo", data)
+        },
+    },
+
+    mutations: {
+        //setters for goals
+        setIncrGoal(state) {
+            state.selectedGoalIndex++
+        },
+        setDecrGoal(state) {
+            state.selectedGoalIndex--
+        },
+        setGoalIndex(state, data) {
+            state.selectedGoalIndex = data
+        },
+
+        //setters for display more goal info
+        setShowGoalInfo(state, data) {
+            state.showGoalInfo = data
+        },
+
+        setAddGoal(state, data) {
+            state.savingGoals.push(data)
+        },
+    },
+
+
 });
