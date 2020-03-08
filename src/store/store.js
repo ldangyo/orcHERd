b48fce2714d4df2c56bca4be7551c8e2f5de8b92
Breyanne <breyannebb@gmail.com>
2020-03-08 01:53:51 -0800

    Implement SavingsCard component

--- src/store/store.js
+++ src/store/store.js
@@ -5,13 +5,21 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
     state: {
-        savingGoals: [{ name: "Korea Vacation", amountSaved: "5", amountReq: "56", category: "Entertainment" }]
-
+        totalMoney: 25,
+        savingGoals: [
+            { name: "Korea Vacation", amountSaved: 5, amountReq: 56, category: "Entertainment", targetDate: new Date("2020-09-28") },
+            { name: "New Pair of Skates", amountSaved: 20, amountReq: 147, category: "Sports", targetDate: new Date("2020-06-01") }
+        ],
+        categories: ['Makeup', 'Clothing', 'Sports', 'Food', 'Entertainment', 'Travel', 'Social', 'Toy', 'Other'],
     },
 
     getters: {
+        getTotalMoney: state => state.totalMoney,
         getSavingGoals: state => {
             return state.savingGoals
+        },
+        getCategories: state => {
+            return state.categories
         }
     },
 
